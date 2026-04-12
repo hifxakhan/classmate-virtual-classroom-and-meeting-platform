8e837083de0f79b5ddeff0306c3fdae6e91ac957
hifxakhan <hifxakhn@gmail.com>
2026-04-12 18:48:48 +0500

    Fixing Transcribing prob...18

--- code/my-react-app/src/ClassMate-Backend/app.py
+++ code/my-react-app/src/ClassMate-Backend/app.py
@@ -1,4 +1,8 @@
-﻿from db import getDbConnection
+﻿import eventlet
+
+eventlet.monkey_patch(socket=False, dns=False)
+
+from db import getDbConnection
 from flask import Flask, send_from_directory, request, jsonify
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit, join_room, leave_room
